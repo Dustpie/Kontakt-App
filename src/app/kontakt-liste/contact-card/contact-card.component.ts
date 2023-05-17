d4b3e2f7b7724fd93cdc3e189f5c300811ec3e01
Dustin Pyka <dustin.pyka@ascaion.com>
2023-05-17 16:22:16 +0200

    have to fix the directory path

--- src/app/kontakt-liste/contact-card/contact-card.component.ts
+++ src/app/kontakt-liste/contact-card/contact-card.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Contact } from '../kontakt-liste/contact-card';
+import { Contacts } from '../mock-contacts';
 
 @Component({
   selector: 'app-contact-card',
   templateUrl: './contact-card.component.html',
   styleUrls: ['./contact-card.component.scss'],
 })
-export class ContactCardComponent {}
+export class ContactCardComponent {
+  deleteContact(contact: Contact): void {
+    let index = Contacts.indexOf(contact);
+    Contacts.splice(index, 1);
+  }
+}
